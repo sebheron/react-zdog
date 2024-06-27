d83babb73606f3df8d007577e3dcd1355a56566e
Seb <sebheron21@hotmail.co.uk>
2024-06-27 17:43:48 +0100

    Add zfont

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import Zdog from 'zdog'
-import { useRender, useInvalidate, useZdog } from './hooks'
-import { createZdog } from './utils'
-import { Illustration } from './Illustration'
+import Zdog from "zdog";
+import { useRender, useInvalidate, useZdog } from "./hooks";
+import { createZdog } from "./utils";
+import { Illustration } from "./Illustration";
 
-const Anchor = createZdog(Zdog.Anchor)
-const Shape = createZdog(Zdog.Shape)
-const Group = createZdog(Zdog.Group)
-const Rect = createZdog(Zdog.Rect)
-const RoundedRect = createZdog(Zdog.RoundedRect)
-const Ellipse = createZdog(Zdog.Ellipse)
-const Polygon = createZdog(Zdog.Polygon)
-const Hemisphere = createZdog(Zdog.Hemisphere)
-const Cylinder = createZdog(Zdog.Cylinder)
-const Cone = createZdog(Zdog.Cone)
-const Box = createZdog(Zdog.Box)
+const Anchor = createZdog(Zdog.Anchor);
+const Shape = createZdog(Zdog.Shape);
+const Group = createZdog(Zdog.Group);
+const Rect = createZdog(Zdog.Rect);
+const RoundedRect = createZdog(Zdog.RoundedRect);
+const Ellipse = createZdog(Zdog.Ellipse);
+const Polygon = createZdog(Zdog.Polygon);
+const Hemisphere = createZdog(Zdog.Hemisphere);
+const Cylinder = createZdog(Zdog.Cylinder);
+const Cone = createZdog(Zdog.Cone);
+const Box = createZdog(Zdog.Box);
+const Text = createZdog(Zdog.Text);
 
 export {
   Illustration,
@@ -31,4 +32,5 @@ export {
   Cylinder,
   Cone,
   Box,
-}
+  Text,
+};
